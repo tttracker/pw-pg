8999d7cdb6b2cc762afa9e2c7ad6f8a3e06c649e
tttracker <123013779+tttracker@users.noreply.github.com>
2023-01-18 19:44:33 +0100

    Add files via upload

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+import * as dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+test('open page and fetch elements', async ({ page, context }) => {
+  const res = await fetch(process.env.fetchUrl as string);
+  const links = await res.json() as string[];
+
+  let dataByLinks = {};
+  const pages = links.map(async (l) => {
+    const page = await context.newPage();
+    await page.goto(l);
+
+    const vEls = await page.$$("a:has(> canvas)");
+    const vLinks = await Promise.all(vEls.map(a => a.getAttribute('href')));
+
+    dataByLinks[l] = vLinks;
+  });
+
+  await Promise.all(pages);
+
+  console.log("dataByLinks: ", dataByLinks)
+  await fetch(
+    process.env.postUrl as string,
+    { method: 'POST', body: JSON.stringify(dataByLinks), headers: { "Content-Type": "application/json" } })
+
+  console.log("Done");
+});
